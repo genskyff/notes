eb8cd76201c3b70ef918de18a9069265e419eb47
Binwalker <gensky377@gmail.com>
2025-02-21 20:27:59 +0900

    updated at 2025/02/21

--- update.bat
+++ update.bat
@@ -1,3 +1,10 @@
+@echo off
+call pnpm run lint
+if errorlevel 1 (
+    echo Lint check failed
+    pause
+    exit /b 1
+)
 git add .
 git commit -m "updated at %date:~0,4%/%date:~5,2%/%date:~8,2%"
 git push
